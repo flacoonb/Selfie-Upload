d48ee57fbc2537d455209c5c6a45bb67498742f0
flacoonb <44100356+flacoonb@users.noreply.github.com>
2024-11-18 21:55:25 +0100

    Update config.php

--- Webserver/config/config.php
+++ Webserver/config/config.php
@@ -1,6 +1,6 @@
 <?php
 return [
-    'base_url' => 'https://XXXX.com', // Basis-URL der Website
+    'base_url' => 'https://XXXX.com/selfie-upload', // Basis-URL der Website
     'photobooth_webhook_url' => 'https://XXXX.com/private/webhook/webhook_receiver.php' // URL der Photobooth
     'delete_image_webhook_url' => 'https://XXXX.com/test/delete_image.php' // Webhook für die Bildlöschung
 ];
